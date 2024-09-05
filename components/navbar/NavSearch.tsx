9802e966923a7f5655c01a082f87fc3a234d226f
Joao Astolfi <jpastolfi@gmail.com>
2024-09-05 15:22:22 +0100

    shadcn Input, NavSearch component, Logo component

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,5 +1,7 @@
+import { Input } from "../ui/input";
+
 export default function NavSearch() {
   return (
-    <div>NavSearch</div>
+    <Input type="text" placeholder="Find a property" className="max-w-xs dark:bg-muted" />
   )
 }
