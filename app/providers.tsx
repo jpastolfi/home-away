52dc4451c099ba7009a6448352b1c0e95a8a8962
Joao Astolfi <jpastolfi@gmail.com>
2024-09-05 15:55:44 +0100

    next-themes, shadcn dropdown, dark mode and providers

---
+++ app/providers.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { ThemeProvider } from './theme-provider'
+
+import React from 'react'
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return <>
+    <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
+      {children}
+    </ThemeProvider>
+  </>
+}
