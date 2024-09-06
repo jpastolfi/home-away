702279dcbd8180754093780e91135cf682dd7dd1
Joao Astolfi <jpastolfi@gmail.com>
2024-09-06 17:30:10 +0100

    Create profile page

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,7 +1,21 @@
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 
+const createProfileAction = async (formData: FormData) => {
+  'use server'
+  const firstName = formData.get('firstName') as string;
+  console.log(firstName);
+}
 
 export default function CreateProfilePage() {
-  return (
-    <div>CreateProfilePage</div>
-  )
+  return <section>
+    <h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
+    <div className="border p-8 rounded-md max-w-lg">
+      <form action={createProfileAction}>
+
+        <Button type="submit" size='lg'>Create profile</Button>
+      </form>
+    </div>
+  </section>
 }
---
+++ components/form/FormInput.tsx
@@ -0,0 +1,20 @@
+import { Input } from '../ui/input'
+import { Label } from '../ui/label'
+
+type FormInputProps = {
+  name: string;
+  type: string;
+  label?: string;
+  defaultValue?: string;
+  placeholder?: string
+}
+
+export default function FormInput(props: FormInputProps) {
+  const { name, type, label, defaultValue, placeholder } = props;
+  return (
+    <div className="mb-2">
+      <Label htmlFor={name} className='capitalize'>{label || name}</Label>
+      <Input id={name} name={name} type={type} defaultValue={defaultValue} placeholder={placeholder}></Input>
+    </div>
+  )
+}
