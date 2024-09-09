36d7f8aeb57832cb5e990177cf484e605d88e57d
Joao Astolfi <jpastolfi@gmail.com>
2024-09-09 13:11:32 +0100

    Profile Model and creation logic in the action

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,19 +1,40 @@
 'use server'
 
 import { ProfileSchema } from "./schemas"
+import db from './db';
+import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
 
 export const createProfileAction = async (prevState: any, formData: FormData) => {
   try {
+    const user = await currentUser();
+    if (!user) throw new Error("Please login to create a profile")
     const rawData = Object.fromEntries(formData);
     const validatedFields = ProfileSchema.parse(rawData);
-    console.log(validatedFields);
-    return {
-      message: 'Profile created'
-    }
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? '',
+        ...validatedFields
+      }
+    })
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      }
+    })
   } catch (error) {
     console.log(error);
+    
     return {
-      message: 'There was an error'
+      message: error instanceof Error ? error.message : "An error occurred."
     }
   }
+  redirect("/");
 }
+
+
+
