d2055d1be70427f16e28bbda87a8e422043f9614
Joao Astolfi <jpastolfi@gmail.com>
2024-09-20 17:09:26 +0100

    Image container. Property rating alignment fix

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,7 @@
 import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
+import PropertyRating from "@/components/card/PropertyRating";
 import BreadCrumbs from "@/components/properties/BreadCrumbs";
+import ImageContainer from "@/components/properties/ImageContainer";
 import ShareButton from "@/components/properties/ShareButton";
 import { fetchPropertyDetails } from "@/utils/actions"
 import { redirect } from "next/navigation";
@@ -19,6 +21,18 @@ export default async function PropertyDetailsPage({ params }: { params: { id: st
           <FavoriteToggleButton propertyId={property.id} />
         </div>
       </header>
+      <ImageContainer mainImage={property.image} name={property.name} />
+      <section className="lg:grid lg:grid-cols-12 gap-x-12 mt-12">
+        <div className="lg:col-span-8">
+          <div className="flex gap-x-4 items-center">
+            <h1 className="text-xl font-bold">{property.name}</h1>
+            <PropertyRating inPage propertyId={property.id} />
+          </div>
+        </div>
+        <div className="lg:col-span-4 flex flex-col items-center">
+          Calendar
+        </div>
+      </section>
     </section>
   )
 }
