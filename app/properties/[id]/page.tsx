b1b57c00384d7b11107eebcee7ea46f04048ba2c
Joao Astolfi <jpastolfi@gmail.com>
2024-09-20 17:49:52 +0100

    Calendar base component

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,6 @@
 import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
 import PropertyRating from "@/components/card/PropertyRating";
+import BookingCalendar from "@/components/properties/BookingCalendar";
 import BreadCrumbs from "@/components/properties/BreadCrumbs";
 import ImageContainer from "@/components/properties/ImageContainer";
 import ShareButton from "@/components/properties/ShareButton";
@@ -30,7 +31,7 @@ export default async function PropertyDetailsPage({ params }: { params: { id: st
           </div>
         </div>
         <div className="lg:col-span-4 flex flex-col items-center">
-          Calendar
+          <BookingCalendar />
         </div>
       </section>
     </section>
