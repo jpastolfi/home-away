d646c23c49489fb6b8f37ee4990af40b43290899
Joao Astolfi <jpastolfi@gmail.com>
2024-09-22 17:23:15 +0100

    Property Details component

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -3,6 +3,7 @@ import PropertyRating from "@/components/card/PropertyRating";
 import BookingCalendar from "@/components/properties/BookingCalendar";
 import BreadCrumbs from "@/components/properties/BreadCrumbs";
 import ImageContainer from "@/components/properties/ImageContainer";
+import PropertyDetails from "@/components/properties/PropertyDetails";
 import ShareButton from "@/components/properties/ShareButton";
 import { fetchPropertyDetails } from "@/utils/actions"
 import { redirect } from "next/navigation";
@@ -29,6 +30,7 @@ export default async function PropertyDetailsPage({ params }: { params: { id: st
             <h1 className="text-xl font-bold">{property.name}</h1>
             <PropertyRating inPage propertyId={property.id} />
           </div>
+          <PropertyDetails details={details} />
         </div>
         <div className="lg:col-span-4 flex flex-col items-center">
           <BookingCalendar />
