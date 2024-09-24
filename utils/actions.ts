2e21be326779ccd3dd81be2329965a753b9d1b9a
Joao Astolfi <jpastolfi@gmail.com>
2024-09-24 13:03:21 +0100

    Reviews page

--- utils/actions.ts
+++ utils/actions.ts
@@ -285,7 +285,24 @@ export const fetchPropertyReviews = async (id: string) => {
 };
 
 export const fetchPropertyReviewsByUser = async () => {
-  return { message: 'fetch user reviews' };
+  const user = await getAuthUser();
+  const reviews = await db.review.findMany({
+    where: {
+      profileId: user.id
+    },
+    select: {
+      id: true,
+      rating: true,
+      comment: true,
+      property: {
+        select: {
+          name: true,
+          image: true
+        }
+      }
+    }
+  })
+  return reviews;
 };
 
 export const deleteReviewAction = async () => {
