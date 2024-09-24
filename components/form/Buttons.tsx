bf75dc49d099e13a576460d5ae49733ed1caab62
Joao Astolfi <jpastolfi@gmail.com>
2024-09-24 14:22:45 +0100

    Reviews page and delete review functionality

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -6,6 +6,7 @@ import { Button } from "../ui/button"
 import { auth } from "@clerk/nextjs/server"
 import { SignInButton } from "@clerk/nextjs"
 import { FaRegHeart, FaHeart } from 'react-icons/fa';
+import { LuTrash, LuPenSquare } from "react-icons/lu"
 
 type btnSize = 'default' | 'lg' | 'sm'
 
@@ -54,5 +55,25 @@ export const CardSubmitButton = ({ isFavorite }: { isFavorite: boolean }) => {
           :
           <FaRegHeart />
     }
+  </Button>
+}
+
+type actionType = 'edit' | 'delete';
+
+export const IconButton = ({ actionType }: { actionType: actionType }) => {
+  const { pending } = useFormStatus();
+  const renderIcon = () => {
+    switch (actionType) {
+      case 'edit':
+        return <LuPenSquare />
+      case 'delete':
+        return <LuTrash />
+      default:
+        const never: never = actionType;
+        throw new Error(`Invalid action type: ${never}`)
+    }
+  }
+  return <Button type="submit" size='icon' variant='link' className="p-2 cursor-pointer">
+    {pending ? <ReloadIcon className="animate-spin" /> : renderIcon()}
   </Button>
 }
