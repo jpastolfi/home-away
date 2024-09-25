aeffeaf60e48d5fefc3fd094b9441738f1cead8f
Joao Astolfi <jpastolfi@gmail.com>
2024-09-25 15:59:03 +0100

    Booking Form with price calculations

--- components/booking/BookingForm.tsx
+++ components/booking/BookingForm.tsx
@@ -1,5 +1,33 @@
+import calculateTotals from "@/utils/calculateTotals";
+import { formatCurrency } from "@/utils/format";
+import { useProperty } from "@/utils/store"
+import { Card, CardTitle } from "../ui/card";
+import { Separator } from "../ui/separator";
+
 export default function BookingForm() {
+  const { range, price } = useProperty(((state) => state));
+  const checkIn = range?.from as Date;
+  const checkOut = range?.to as Date;
+  const { totalNights, subTotal, cleaningFee, tax, orderTotal } = calculateTotals({ checkIn, checkOut, price });
+
   return (
-    <div>BookingForm</div>
+    <Card className="p-8 mb-4">
+      <CardTitle className="mb-8">Summary</CardTitle>
+      <FormRow label={`$${price} x ${totalNights} nights `} amount={subTotal} />
+      <FormRow label={'Cleaning fee'} amount={cleaningFee} />
+      <FormRow label={'Tax'} amount={tax} />
+      <Separator className="mt-4" />
+      <CardTitle className="mt-8">
+        <FormRow label={'Total'} amount={orderTotal} />
+      </CardTitle>
+    </Card>
   )
+}
+
+const FormRow = ({ label, amount }: { label: string, amount: number }) => {
+  return <p className="flex justify-between text-sm mb-2">
+    <span>{label}</span>
+    <span>{formatCurrency(amount)}</span>
+  </p>
+
 }
