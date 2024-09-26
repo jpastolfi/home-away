01085836ca4988581a88c46e288aa440bf3a6985
Joao Astolfi <jpastolfi@gmail.com>
2024-09-26 13:29:00 +0100

    Added toast message when trying to book dates that are already booked

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -18,8 +18,24 @@ export default function BookingCalendar() {
   const currentDate = new Date();
   const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
   const { bookings } = useProperty((state) => state);
+  const { toast } = useToast();
+
   const blockedPeriods = generateBlockedPeriods({ bookings, today: currentDate });
+
+  const unavailableDates = generateDisabledDates(blockedPeriods);
+
   useEffect(() => {
+    const selectedRange = generateDateRange(range);
+    const isDisabledDateIncluded = selectedRange.some((date) => {
+      if (unavailableDates[date]) {
+        setRange(defaultSelected);
+        toast({
+          description: "Some dates are booked, please select again"
+        });
+        return true;
+      }
+      return false;
+    });
     useProperty.setState({ range })
   }, [range])
   return (
