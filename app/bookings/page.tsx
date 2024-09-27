503a61649387a114b08eaead01f5baac942b9d49
Joao Astolfi <jpastolfi@gmail.com>
2024-09-27 09:24:47 +0100

    Booking details using Table

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,5 +1,49 @@
-export default function BookingsPage() {
+import CountryFlagAndName from "@/components/card/CountryFlagAndName";
+import EmptyList from "@/components/home/EmptyList";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { fetchBookins } from "@/utils/actions"
+import { formatCurrency, formatDate } from "@/utils/format";
+import Link from "next/link";
+
+
+export default async function BookingsPage() {
+  const bookings = await fetchBookins();
+  if (bookings.length < 1) return <EmptyList />
+
   return (
-    <div className="text-3xl">BookingsPage</div>
+    <div className="mt-16">
+      <h4 className="mb-4 capitalize">Total bookings: {bookings.length}</h4>
+      <Table>
+        <TableCaption>A list of your recent bookings</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Property Name  </TableHead>
+            <TableHead>Country</TableHead>
+            <TableHead>Nights</TableHead>
+            <TableHead>Total</TableHead>
+            <TableHead>Check In</TableHead>
+            <TableHead>Check Out</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {bookings.map((booking) => {
+            const { id, orderTotal, totalNights, checkIn, checkOut } = booking;
+            const { id: propertyId, name, country } = booking.property;
+            const startDate = formatDate(checkIn);
+            const endDate = formatDate(checkOut);
+            return <TableRow key={id}>
+              <TableCell><Link href={`/properties/${propertyId}`} className="text-muted-foreground tracking-wide">{name}</Link></TableCell>
+              <TableCell><CountryFlagAndName countryCode={country} /></TableCell>
+              <TableCell>{totalNights}</TableCell>
+              <TableCell>{formatCurrency(orderTotal)}</TableCell>
+              <TableCell>{startDate}</TableCell>
+              <TableCell>{endDate}</TableCell>
+
+            </TableRow>
+          })}
+        </TableBody>
+      </Table>
+    </div>
   )
 }
--- utils/actions.ts
+++ utils/actions.ts
@@ -390,3 +390,42 @@ export const createBookingAction = async (prevState: {
   }
   redirect('/bookings')
 }
+
+export const fetchBookins = async () => { 
+  const user = await getAuthUser();
+  const bookings = await db.booking.findMany({
+    where: {
+      profileId: user.id
+    },
+    include: {
+      property: {
+        select: {
+          id: true,
+          name: true,
+          country: true
+        }
+      }
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  });
+  return bookings;
+}
+
+export const deleteBooking = async (prevState: {bookingId: string}) => {
+  const {bookingId} = prevState;
+  const user = await getAuthUser();
+  try {
+    const result = await db.booking.delete({
+      where: {
+        id: bookingId,
+        profileId: user.id
+      }
+    })
+    revalidatePath('/bookings')
+    return {message: "Booking deleted successfully"};
+  } catch (error) {
+    renderError(error)
+  }
+}
